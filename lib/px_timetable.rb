6cc6b1a4e419a0741c2a23162342be5097b334ed
Your Name <you@example.com>
2016-09-19 19:14:45 +0100

    1st commit

---
+++ lib/px_timetable.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+
+# file: px_timetable.rb
+
+
+require 'polyrex'
+require 'chronic_between'
+
+
+
+class PxTimetable
+
+  def initialize(filename_txt)
+    @px = Polyrex.new
+    @px.import filename_txt
+  end
+
+  def next(t=Time.now)
+
+    classes_today = self.today()
+    return unless classes_today
+
+    classes = classes_today.records    
+
+    session = classes.detect do |klass|
+      ChronicBetween.new(klass.times).within? t
+    end
+
+    i = classes.index(session)
+
+    if i and i < classes.length then
+      classes[i+1]
+    end
+
+  end
+
+  def now(t=Time.now)
+
+    classes_today = self.today()
+    return unless classes_today
+
+    classes = classes_today.records    
+
+    classes.detect do |klass|
+      ChronicBetween.new(klass.times).within? t
+    end
+  end
+
+  def to_s()
+    @px.to_s(header: false)
+  end
+
+  def today(d=Date.today)
+    
+    @px.find_by_day_title Date::DAYNAMES[d.wday]
+
+  end
+  
+  def tomorrow(d=Date.today+1)
+    
+    @px.find_by_day_title Date::DAYNAMES[(d).wday]
+
+  end
+
+  
+end
+
+
---
+++ px_timetable.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'px_timetable'
+  s.version = '0.1.0'
+  s.summary = 'Query a timetable using a Polyrex document'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/px_timetable.rb']
+  s.add_runtime_dependency('chronic_between', '~> 0.3', '>=0.3.0') 
+  s.signing_key = '../privatekeys/px_timetable.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/px_timetable'
+end
